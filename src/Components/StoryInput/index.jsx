843ea0437e5f3fa156b97988f1b158d56cf6c5b6
Smita <Smita_Jha@mckinsey.com>
2023-03-13 14:53:03 +0530

    feat: Story Input Style Changes

--- src/Components/StoryInput/index.jsx
+++ src/Components/StoryInput/index.jsx
@@ -1,7 +1,9 @@
 import { Button, Box, Input, Fab } from '@mui/material';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faEdit, faTrash } from '@fortawesome/free-solid-svg-icons';
 import PropTypes from 'prop-types';
 import React, { useState } from 'react';
-
+import './StoryInput.css';
 function Item(props) {
   const { sx, ...other } = props;
   return (
@@ -64,111 +66,122 @@ export default function StoryInput({ storyList, setStoryList }) {
   };
   return (
     <>
-      <Box mx="auto" sx={{ bgcolor: 'text.disabled', borderRadius: 2, mt: 2 }}>
-        <div>
-          <Box
-            sx={{
-              display: 'grid',
-              gridTemplateColumns: 'repeat(4, 2fr) 1fr',
-              ml: 6,
-              p: 4,
-            }}
-          >
-            <Item>Stories</Item>
-            <Item>Dependencies</Item>
-            <Item>Developer</Item>
-            <Item>Story Points</Item>
-          </Box>
-          {storyList.length === 0 ? (
-            <h1>No Stories</h1>
-          ) : (
-            storyList.map((story) => {
-              const { id, stories, dependencies, developer, storyPoints } =
-                story;
-              return (
-                <>
-                  <Box
-                    sx={{
-                      display: 'grid',
-                      gridTemplateColumns: 'repeat(4, 2fr) 1fr',
-                      ml: 6,
-                      p: 4,
-                    }}
-                  >
-                    <Item>{stories}</Item>
-                    <Item>{dependencies.toString()}</Item>
-                    <Item>{developer}</Item>
-                    <Item>{storyPoints}</Item>
-                    <Fab
-                      color="primary"
-                      onClick={() => removeItem(id)}
-                      aria-label="add"
-                    >
-                      -
-                    </Fab>
-                    {/* // <Button  onClick={() => removeItem(id)}>Remove</Button> */}
-                  </Box>
-                </>
-              );
-            })
-          )}
-        </div>
-        <form onSubmit={handleSubmit}>
-          <Box
-            sx={{
-              display: 'grid',
-              gridTemplateColumns: 'repeat(4, 2fr) 1fr',
-              ml: 6,
-              p: 4,
-            }}
-          >
-            <Item>
-              <Input
-                type="text"
-                name="story"
-                defaultValue="Disabled"
-                value={stories}
-                placeholder="Enter Story Title"
-                onChange={(e) => setStories(e.target.value)}
-              />
-            </Item>
-            <Item>
-              <Input
-                type="text"
-                name="dependencies"
-                value={dependencies}
-                placeholder="Enter Dependencies"
-                onChange={(e) => setDependencies(e.target.value)}
-              />
-            </Item>
-            <Item>
-              <Input
-                type="text"
-                name="developers"
-                value={developer}
-                placeholder="Enter Developer"
-                onChange={(e) => setDeveloper(e.target.value)}
-              />
-            </Item>
-            <Item>
-              <Input
-                type="text"
-                name="storyPoints"
-                value={storyPoints}
-                placeholder="Enter Story Points"
-                onChange={(e) => setStoryPoints(e.target.value)}
-              />
-            </Item>
-          </Box>
-          <Button
-            variant="contained"
-            style={{ margin: '0 auto', display: 'flex' }}
-            type="submit"
-          >
-            Add Story
-          </Button>
-        </form>
-      </Box>
+      <div className="story-container">
+        <Box mx="auto">
+          <div>
+            <Box
+              sx={{
+                display: 'grid',
+                gridTemplateColumns: 'repeat(4, 2fr) 1fr 1fr',
+                ml: 6,
+                p: 1,
+              }}
+            >
+              <Item>Stories</Item>
+              <Item>Dependencies</Item>
+              <Item>Developer</Item>
+              <Item>Story Points</Item>
+            </Box>
+            <div className="story-list">
+              {storyList.length === 0 ? (
+                <h1>No Stories</h1>
+              ) : (
+                storyList.map((story) => {
+                  const { id, stories, dependencies, developer, storyPoints } =
+                    story;
+                  return (
+                    <>
+                      <Box
+                        sx={{
+                          display: 'grid',
+                          gridTemplateColumns: 'repeat(4, 2fr) 1fr 1fr',
+                          backgroundColor: 'white',
+                          borderRadius: '10px',
+                          ml: 6,
+                          mt: 1,
+                          p: 1,
+                        }}
+                      >
+                        <Item>{stories}</Item>
+                        <Item>{dependencies.toString()}</Item>
+                        <Item>{developer}</Item>
+                        <Item>{storyPoints}</Item>
+                        <Fab
+                          color="primary"
+                          onClick={() => removeItem(id)}
+                          aria-label="add"
+                        >
+                          <FontAwesomeIcon icon={faTrash} />
+                        </Fab>
+                        <Fab>
+                          <FontAwesomeIcon icon={faEdit} />
+                        </Fab>
+                        {/* // <Button  onClick={() => removeItem(id)}>Remove</Button> */}
+                      </Box>
+                    </>
+                  );
+                })
+              )}
+            </div>
+          </div>
+          <form className="story-container-form" onSubmit={handleSubmit}>
+            <Box
+              sx={{
+                display: 'grid',
+                gridTemplateColumns: 'repeat(4, 2fr) 1fr 1fr',
+                ml: 6,
+                p: 1,
+              }}
+            >
+              <Item>
+                <Input
+                  type="text"
+                  name="story"
+                  defaultValue="Disabled"
+                  value={stories}
+                  placeholder="Enter Story Title"
+                  onChange={(e) => setStories(e.target.value)}
+                />
+              </Item>
+              <Item>
+                <Input
+                  type="text"
+                  name="dependencies"
+                  value={dependencies}
+                  placeholder="Enter Dependencies"
+                  onChange={(e) => setDependencies(e.target.value)}
+                />
+              </Item>
+              <Item>
+                <Input
+                  type="text"
+                  name="developers"
+                  value={developer}
+                  placeholder="Enter Developer"
+                  onChange={(e) => setDeveloper(e.target.value)}
+                />
+              </Item>
+              <Item>
+                <Input
+                  type="text"
+                  name="storyPoints"
+                  value={storyPoints}
+                  placeholder="Enter Story Points"
+                  onChange={(e) => setStoryPoints(e.target.value)}
+                />
+              </Item>
+              <div></div>
+              <Fab
+                variant="contained"
+                style={{ margin: '0 auto', display: 'flex' }}
+                type="submit"
+              >
+                +
+              </Fab>
+            </Box>
+          </form>
+        </Box>
+      </div>
     </>
   );
 }
--- src/Components/Title/index.jsx
+++ src/Components/Title/index.jsx
@@ -3,14 +3,48 @@ import {
   Box,
   createTheme,
   responsiveFontSizes,
+  TextField,
 } from '@mui/material';
+import PropTypes from 'prop-types';
+import EditIcon from '../../assets/user-pencil-write-ui-education_2023-03-09/user-pencil-write-ui-education@2x.png';
 
-import React from 'react';
+import React, { useState } from 'react';
 
 let theme = createTheme();
 theme = responsiveFontSizes(theme);
 
+const InlineEdit = ({ value, setValue }) => {
+  const [editingValue, setEditingValue] = useState(value);
+
+  const onChange = (event) => setEditingValue(event.target.value);
+
+  const onKeyDown = (event) => {
+    if (event.key === 'Enter' || event.key === 'Escape') {
+      event.target.blur();
+    }
+  };
+
+  const onBlur = (event) => {
+    setValue(event.target.value);
+  };
+
+  return (
+    <TextField
+      id="standard-basic"
+      variant="standard"
+      type="text"
+      placeholder="Enter a title"
+      aria-label="Field name"
+      value={editingValue}
+      onChange={onChange}
+      onKeyDown={onKeyDown}
+      onBlur={onBlur}
+    />
+  );
+};
+
 export default function Title() {
+  const [value, setValue] = useState();
   return (
     <Box
       component="span"
@@ -22,8 +56,19 @@ export default function Title() {
       bgcolor="lightblue"
     >
       <Typography variant="h4" theme={theme} m="auto" gutterBottom>
-        Project Title
+        <InlineEdit
+          value={value}
+          variant="h4"
+          theme={theme}
+          setValue={setValue}
+        />
+        <img src={EditIcon}></img>
       </Typography>
     </Box>
   );
 }
+
+InlineEdit.propTypes = {
+  value: PropTypes.string.isRequired,
+  setValue: PropTypes.func.isRequired,
+};
